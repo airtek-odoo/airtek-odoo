a7eedcc1ffc1ca0c1f0e52eabfed7b5f18bb8365
rhe-mastercore <rhe@sinapsys.global>
2021-10-20 11:23:08 -0400

    [UPD] merge dev

--- archivo.py
+++ archivo.py
@@ -7,19 +7,34 @@
 #
 ###############################################################################
 #Comentario
-def comentarios()
+def comentarios():
+    pass
+#me gusta el estudio
+def comentarios():
     print ('hola Joas') #comentario del print
     print ('Jugaremos muevete luz verde...') # ronald romero
     print ('hola GUapos') # gege XD
     print ('El ser Humano es Bueno o Malo por Naturaleza?') #pregunta de media noche
     print ('hola [CAMARADAS!]') #->Joas
     print ('wpadilla')# ->wpadilla
-    print ('El ser Humano es Bueno o Malo por Naturaleza?') 
+    print ('El ser Humano no es Bueno o Malo por Naturaleza?') 
     print('otravez yo ' )# ->wpadilla
     print('#line17 R: EL humano es malo por naturaleza, el razonamiento y el afecto de terceros es el que lo amansa')
 
-def WillEstaMal()
+def WillEstaMal():
+    print("otro yo ")# ->wpadilla
+def WillEstaMal():
     print('Will es lokote')
+    print('Pruebas Merg')
+def nuevarama()
+    print('Pruebas Merg')
 
+def scrollingwp()
+    print("wola")
+
+def compressedList(arg):
+    x = [print(x) for x in arg if not arg.isdigit()]
+    print(x)
+    return x
 def reydiFunction()
     print('Hello world!')
