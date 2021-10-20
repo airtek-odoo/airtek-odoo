845346b9ded0f33972d6d4e8a94ff5b3a7bcad75
rhe-mastercore <rhe@sinapsys.global>
2021-10-20 10:10:08 -0400

    [ADD] include function

--- archivo.py
+++ archivo.py
@@ -7,22 +7,11 @@
 #
 ###############################################################################
 #Comentario
-print ('hola Joas') #comentario del print
-<<<<<<< HEAD
-<<<<<<< HEAD
-print ('Jugaremos muevete luz verde...') # ronald romero
-=======
-print ('hola GUapos') # gege XD
->>>>>>> 11ec06f5b98487f36caaac30fea5e971d6483733
-=======
-
-print ('hola GUapos') # gege XD
-
-
-
-print ('El ser Humano es Bueno o Malo por Naturaleza?') #pregunta de media noche
-
-print ('hola [CAMARADAS!]') #->Joas
-print ('wpadilla')# ->wpadilla
-print ('El ser Humano es Bueno o Malo por Naturaleza?') 
->>>>>>> 8900cf717a0278fff574b5ad3da4f1b92926d9c0
+def comentarios()
+    print ('hola Joas') #comentario del print
+    print ('Jugaremos muevete luz verde...') # ronald romero
+    print ('hola GUapos') # gege XD
+    print ('El ser Humano es Bueno o Malo por Naturaleza?') #pregunta de media noche
+    print ('hola [CAMARADAS!]') #->Joas
+    print ('wpadilla')# ->wpadilla
+    print ('El ser Humano es Bueno o Malo por Naturaleza?') 
